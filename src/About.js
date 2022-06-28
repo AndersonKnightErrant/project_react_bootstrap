1d0fe1ec8bcca4a050c0b3512be71f25f7273290
AndersonKnightErrant <andrew83anderson@gmail.com>
2022-06-28 23:48:32 +0400

    add card to Home.js

--- src/About.js
+++ src/About.js
@@ -1,8 +1,26 @@
+import fox from "./img/fox.jpg"
+
 function About() {
 	return (
 		<>
-			<div className="About" style={{ "height": "600px" }} >
+			<div className="About" style={{
+				"height": "600px",
+				
+			}} >
 				<h1>About</h1>
+				<br />
+				<div style={{
+					"max-width": "500px",
+					"margin": "0 auto"
+				}}>
+					<p >Данный проект чисто эксперементальный, частично подсмотренный, частично дополненый и никакой интелектуальной собственности не имеет. Может кому он поможет ответить на некоторые вопросы. Я же просто дурачюсь. Кстати. Прикольная лиса на заставке блюющая радугой :) </p>
+				</div>
+				<div>
+					<img src={fox} alt="" style={{
+						"width": "400px",
+						"margin": "20px 0px"
+					}} />
+				</div>
 			</div>
 		</>
 	);
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,58 @@
 import React from "react";
 import Slider from "./Slider";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import fox from "./img/fox.jpg"
 
 function Home() {
 	return (
 		<>
-			<Slider/>
+			<Slider />
+			<Container style={{
+				paddingTop: "2rem",
+				paddingBottom: "2rem"
+			}}>
+				<Row>
+					<Col>
+						<Card style={{
+							width: "18rem",
+							border: "2px solid black"
+						}}>
+							<Card.Img variant="top" src={fox} />
+							<Card.Body>
+								<Card.Title>AndersonBlog. Цитаты</Card.Title>
+								<Card.Text>Он никогда не сдается заранее. Как бы не было тяжело, как бы не хотелось все бросить, он все равно сделает шаг вперед, забыв эти мысли.</Card.Text>
+							</Card.Body>
+							<Button variant="primary">Show more</Button>
+						</Card>
+					</Col>
+					<Col>
+						<Card style={{
+							width: "18rem",
+							border: "2px solid black"
+						}}>
+							<Card.Img variant="top" src={fox} />
+							<Card.Body>
+								<Card.Title>AndersonBlog. Цитаты</Card.Title>
+								<Card.Text>Проигрыш доказывает, что вы слабы? Проигрыш отягощает вас? Разве вы не должны после поражения снова встать? Если останетесь на коленях — это докажет, что вы слабы.</Card.Text>
+							</Card.Body>
+							<Button variant="primary">Show more</Button>
+						</Card>
+					</Col>
+					<Col>
+						<Card style={{
+							width: "18rem",
+							border: "2px solid black"
+						}}>
+							<Card.Img variant="top" src={fox} />
+							<Card.Body>
+								<Card.Title>AndersonBlog. Цитаты</Card.Title>
+								<Card.Text>Вместо того, чтобы сдаваться и отчаиваться, что ты не гений. Верь, что твоя сила этим не ограничивается, и продолжай идти всегда вперед.</Card.Text>
+							</Card.Body>
+							<Button variant="primary">Show more</Button>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
 		</>
 	);
 }
